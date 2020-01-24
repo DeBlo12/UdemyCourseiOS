cc8e50715cd6df75a00bf802ab9fb194dba19922
Ivo Specht <ivo.specht@mail.com>
2020-01-24 22:45:42 +0000

    Started with Clima app

--- Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,64 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     
+    //txtfields
+    @IBOutlet weak var searchTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchTF.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTF.endEditing(true)
+        guard let searchedCity = searchTF.text else {return}
+        print(searchedCity)
+        
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTF.endEditing(true)
+        print(searchTF.text!)
+        
+        return true
+    }
+    
+    
+    // Use general textField here, as it would jump to the selected textField through the system.
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Write something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        textField.text = ""
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func currentLocationPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
 }
 
