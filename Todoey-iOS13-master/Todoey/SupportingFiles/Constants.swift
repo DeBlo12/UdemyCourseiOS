d11c9ed545fe98cda41d2946e6e6dae0e225e180
Ivo Specht <ivo.specht@mail.com>
2020-03-19 21:16:53 +0000

    Add Tableview and set it up initial.

---
+++ Todoey-iOS13-master/Todoey/SupportingFiles/Constants.swift
@@ -0,0 +1,23 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Ivo Specht on 19/03/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    
+    struct TableviewComponents {
+        
+        static let cellReusableIdentifier = "ToDoItemCell"
+        
+    }
+    
+    
+    
+}
